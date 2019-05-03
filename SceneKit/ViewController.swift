404c7469a902b8e9b41becee3eb18c3c6a2b7864
Rawand Saeed <rawand.saeed@gmail.com>
2019-05-03 11:28:54 +0300

    Subject: Issue
    Changed the name of the project to avoid the module conflict when importing SceneKit module.

--- SceneKit/ViewController.swift
+++ SceneKit/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadSceneView()
+    }
+    
+    func loadSceneView(){
+        
+        // Step 1 -- load scene view to the viewcontroller
+        
+        let sceneView = self.view as! SCNView
+        sceneView.backgroundColor = UIColor.gray
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
     }
 
 
